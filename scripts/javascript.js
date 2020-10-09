1641a3802ba4ad2f3e2405aea5d8fab698619428
Chase Simmons <csim6890@gmail.com>
2020-10-09 15:53:52 -0500

    js functions added

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -2,4 +2,27 @@ $('document').ready(readyNow);
 
 function readyNow() {
   console.log('Document is ready.');
+  $('#submitButton').on('click', submitter);
 }
+
+function submitter() {
+  if (checkFields() === true) {
+    console.log('working');
+  }
+}
+function checkFields() {
+  if (
+    $(firstNameInput).val() === '' ||
+    $(lastNameInput).val() === '' ||
+    $(idInput).val() === '' ||
+    $(titleInput).val() === '' ||
+    $(salaryInput).val() === ''
+  ) {
+    alert('Please fill out all fields before submitting.');
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function updateTotalMonthly() {}

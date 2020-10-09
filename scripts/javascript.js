c845f5249e0ff5b1fdb262f0464ec9a86af15391
Chase Simmons <csim6890@gmail.com>
2020-10-09 16:38:41 -0500

    functions working added deleter function

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,13 +1,24 @@
 $('document').ready(readyNow);
 
+let totalMonthlySalaryValue = 0;
+
 function readyNow() {
   console.log('Document is ready.');
   $('#submitButton').on('click', submitter);
+  $('#tableBody').on('click', '.js-btn', deleter);
 }
 
 function submitter() {
   if (checkFields() === true) {
-    console.log('working');
+    $('#tableBody').append(`<tr> 
+    <td>${$(firstNameInput).val()}</td>
+    <td>${$(lastNameInput).val()}</td>
+    <td>${$(idInput).val()}</td>
+    <td>${$(titleInput).val()}</td>
+    <td>$${$(salaryInput).val()}</td>
+    <td><button class="js-btn">Delete</button></td>
+    </tr>`);
+    updateTotalMonthly();
   }
 }
 function checkFields() {
@@ -25,4 +36,14 @@ function checkFields() {
   }
 }
 
-function updateTotalMonthly() {}
+function updateTotalMonthly() {
+  totalMonthlySalaryValue = parseInt(
+    Number($(salaryInput).val()) / 12 + +totalMonthlySalaryValue
+  );
+  $('#totalMonthly').empty();
+  $('#totalMonthly').append(totalMonthlySalaryValue);
+}
+
+function deleter() {
+  console.log('running');
+}
